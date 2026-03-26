eb3951fd1f6b73f13083a64e3ce206a2c339e2b8
winter123 <2453101190@qq.com>
2026-03-26 18:37:35 +0800

    将SysJob和SysJobLog实现从mabatis更新为jdbc

---
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/repository/impl/SysJobLogRepositoryImpl.java
@@ -0,0 +1,172 @@
+package com.ruoyi.quartz.repository.impl;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.jdbc.core.SimplePropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Service;
+
+import com.ruoyi.common.core.db.DBService;
+import com.ruoyi.common.core.db.parameter.NamedSqlParameterSource;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.quartz.domain.SysJobLog;
+import com.ruoyi.quartz.repository.SysJobLogRepository;
+
+@Service
+public class SysJobLogRepositoryImpl implements SysJobLogRepository {
+
+    private DBService dbService;
+
+    private String baseSelectSql = "SELECT job_log_id, job_name, job_group, invoke_target, job_message, status, exception_info, start_time, end_time, create_time FROM sys_job_log WHERE 1=1";
+
+    public SysJobLogRepositoryImpl(DBService inDbService) {
+        this.dbService = inDbService;
+    }
+
+    protected List<SysJobLog> queryList(final MapSqlParameterSource parameters, final String querySql) {
+        List<SysJobLog> list = dbService.queryForList(querySql, parameters, new SimplePropertyRowMapper<>(SysJobLog.class));
+        return list;
+    }
+
+    @Override
+    public List<SysJobLog> selectJobLogList(SysJobLog jobLog) {
+        StringBuilder sqlBuilder = new StringBuilder(baseSelectSql);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+
+        setListSqlAndParams(jobLog, sqlBuilder, parameters);
+
+        sqlBuilder.append(" ORDER BY job_log_id DESC");
+
+        return queryList(parameters, sqlBuilder.toString());
+    }
+
+    @Override
+    public TableDataInfo getPagedListResp(SysJobLog jobLog) {
+        StringBuilder sqlBuilder = new StringBuilder();
+        NamedSqlParameterSource parameters = new NamedSqlParameterSource();
+
+        setListSqlAndParams(jobLog, sqlBuilder, parameters);
+
+        String selectCountSql = "SELECT COUNT(1) FROM sys_job_log WHERE 1=1";
+        String queryCountSql = selectCountSql + sqlBuilder.toString();
+
+        TableDataInfo pagedResp = dbService.getPagedRespInfo(queryCountSql, parameters);
+
+        parameters.setDefaultOrderByStr("job_log_id DESC");
+
+        dbService.buildPagedSqlAndSetParameters(sqlBuilder, parameters);
+
+        String queryListSql = baseSelectSql + sqlBuilder.toString();
+
+        pagedResp.setRows(queryList(parameters, queryListSql));
+        return pagedResp;
+    }
+
+    private void setListSqlAndParams(final SysJobLog inJobLog, StringBuilder inBuilder, MapSqlParameterSource inParameters) {
+        String jLogName = inJobLog.getJobName();
+        String jLogGroup = inJobLog.getJobGroup();
+        String jLogStatus = inJobLog.getStatus();
+        String jLogInvokeTarget =inJobLog.getInvokeTarget();
+        String beginDateTime = inJobLog.getBeginTimeParam();
+        String endDateTime = inJobLog.getEndTimeParam();
+
+        if(StringUtils.isNotEmpty(jLogName)) {
+            inBuilder.append(" AND job_name LIKE CONCAT('%', :inJLogName, '%')");
+            inParameters.addValue("inJLogName", jLogName);
+        }
+        if(StringUtils.isNotEmpty(jLogGroup)) {
+            inBuilder.append(" AND job_group=:inJLogGroup");
+            inParameters.addValue("inJLogGroup", jLogGroup);
+        }
+        if(StringUtils.isNotEmpty(jLogStatus)) {
+            inBuilder.append(" AND status=:inJLogStatus");
+            inParameters.addValue("inJLogStatus", jLogStatus);
+        }
+        if(StringUtils.isNotEmpty(jLogInvokeTarget)) {
+            inBuilder.append(" AND invoke_target LIKE CONCAT('%', :inJLogInTarget, '%')");
+            inParameters.addValue("inJLogInTarget", jLogInvokeTarget);
+        }
+        if(StringUtils.isNotEmpty(beginDateTime)) {
+            inBuilder.append(" AND create_time >= :inBeginTime");
+            inParameters.addValue("inBeginTime", beginDateTime);
+        }
+        if(StringUtils.isNotEmpty(endDateTime)) {
+            inBuilder.append(" AND create_time <= :inEndTime");
+            inParameters.addValue("inEndTime", endDateTime);
+        }
+    }
+
+    @Override
+    public List<SysJobLog> selectJobLogAll() {
+        return queryList(null, baseSelectSql);
+    }
+
+    @Override
+    public SysJobLog selectJobLogById(Long jobLogId) {
+        String sql = baseSelectSql + " AND job_log_id=:inJLogId";
+
+        MapSqlParameterSource parameters = new MapSqlParameterSource("inJLogId", jobLogId);
+
+        SysJobLog queryObj = dbService.queryForObject(sql, parameters, new SimplePropertyRowMapper<>(SysJobLog.class));
+        return queryObj;
+    }
+
+    @Override
+    public int insertJobLog(SysJobLog jobLog) {
+        String jLogName = jobLog.getJobName();
+        String jLogGroup = jobLog.getJobGroup();
+        String jLogInvokeTarget =jobLog.getInvokeTarget();
+        String jLogMessage = jobLog.getJobMessage();
+        String jLogStatus = jobLog.getStatus();
+        String jLogExcep = StringUtils.isEmpty(jobLog.getExceptionInfo()) ? "" : jobLog.getExceptionInfo();
+        Date jLogStartTime = jobLog.getStartTime();
+        Date jLogEndTime = jobLog.getEndTime();
+        LocalDateTime createTime = LocalDateTime.now(ZoneId.of("UTC"));
+
+        String insertSql = "INSERT INTO sys_job_log(job_name, job_group, invoke_target, job_message, status, exception_info, start_time, end_time, create_time) VALUES(:inJLogName, :inJLogGro, :inJLogInv, :inJLogMess, :inJLogStatus, :inJLogExcep, :inJLogStart, :inJLogEnd, :inCreateTime)";
+
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("inJLogName", jLogName);
+        parameters.addValue("inJLogGro", jLogGroup);
+        parameters.addValue("inJLogInv", jLogInvokeTarget);
+        parameters.addValue("inJLogMess", jLogMessage);
+        parameters.addValue("inJLogStatus", jLogStatus);
+        parameters.addValue("inJLogExcep", jLogExcep);
+        parameters.addValue("inJLogStart", jLogStartTime);
+        parameters.addValue("inJLogEnd", jLogEndTime);
+        parameters.addValue("inCreateTime", createTime);
+
+        int[] insertResList = dbService.batchUpdate(insertSql, new MapSqlParameterSource[]{parameters});
+        return insertResList[0];
+    }
+
+    @Override
+    public int deleteJobLogByIds(Long[] logIds) {
+        String deleteSql = "DELETE FROM sys_job_log WHERE job_log_id=:inJLogId";
+
+        MapSqlParameterSource[] parametersList = new MapSqlParameterSource[logIds.length];
+        for (int i = 0; i < parametersList.length; i++) {
+            Long jLogId = logIds[i];
+            parametersList[i] = new MapSqlParameterSource("inJLogId", jLogId);
+        }
+
+        int[] insertResList = dbService.batchUpdate(deleteSql, parametersList);
+        return insertResList[0];
+    }
+
+    @Override
+    public int deleteJobLogById(Long jobLogId) {
+        return this.deleteJobLogByIds(new Long[]{jobLogId});
+    }
+
+    @Override
+    public void cleanJobLog() {
+        String clearSql = "TRUNCATE TABLE sys_job_log";
+
+        dbService.execute(clearSql);
+    }
+}
--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/service/impl/SysJobServiceImpl.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/service/impl/SysJobServiceImpl.java
@@ -2,6 +2,8 @@ package com.ruoyi.quartz.service.impl;
 
 import java.util.List;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+
 import org.quartz.JobDataMap;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
@@ -10,16 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.ruoyi.common.constant.ScheduleConstants;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.exception.job.TaskException;
 import com.ruoyi.quartz.domain.SysJob;
-import com.ruoyi.quartz.mapper.SysJobMapper;
+import com.ruoyi.quartz.repository.SysJobRepository;
 import com.ruoyi.quartz.service.ISysJobService;
 import com.ruoyi.quartz.util.CronUtils;
 import com.ruoyi.quartz.util.ScheduleUtils;
 
 /**
  * 定时任务调度信息 服务层
- * 
+ *
  * @author ruoyi
  */
 @Service
@@ -28,8 +31,8 @@ public class SysJobServiceImpl implements ISysJobService
     @Autowired
     private Scheduler scheduler;
 
-    @Autowired
-    private SysJobMapper jobMapper;
+    @Resource
+    private SysJobRepository jobRepository;
 
     /**
      * 项目启动时，初始化定时器 主要是防止手动修改数据库导致未同步到定时任务处理（注：不能手动修改数据库ID和任务组名，否则会导致脏数据）
@@ -38,7 +41,7 @@ public class SysJobServiceImpl implements ISysJobService
     public void init() throws SchedulerException, TaskException
     {
         scheduler.clear();
-        List<SysJob> jobList = jobMapper.selectJobAll();
+        List<SysJob> jobList = jobRepository.selectJobAll();
         for (SysJob job : jobList)
         {
             ScheduleUtils.createScheduleJob(scheduler, job);
@@ -47,31 +50,42 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 获取quartz调度器的计划任务列表
-     * 
+     *
      * @param job 调度信息
      * @return
      */
     @Override
     public List<SysJob> selectJobList(SysJob job)
     {
-        return jobMapper.selectJobList(job);
+        return jobRepository.selectJobList(job);
+    }
+
+    /**
+     * 根据条件分页查询调度任务
+     *
+     * @param job 调度任务信息
+     * @return 分页完成的调度任务集合
+     */
+    @Override
+    public TableDataInfo getPagedListResp(SysJob job) {
+        return jobRepository.getPagedListResp(job);
     }
 
     /**
      * 通过调度任务ID查询调度信息
-     * 
+     *
      * @param jobId 调度任务ID
      * @return 调度任务对象信息
      */
     @Override
     public SysJob selectJobById(Long jobId)
     {
-        return jobMapper.selectJobById(jobId);
+        return jobRepository.selectJobById(jobId);
     }
 
     /**
      * 暂停任务
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -81,7 +95,7 @@ public class SysJobServiceImpl implements ISysJobService
         Long jobId = job.getJobId();
         String jobGroup = job.getJobGroup();
         job.setStatus(ScheduleConstants.Status.PAUSE.getValue());
-        int rows = jobMapper.updateJob(job);
+        int rows = jobRepository.updateJob(job);
         if (rows > 0)
         {
             scheduler.pauseJob(ScheduleUtils.getJobKey(jobId, jobGroup));
@@ -91,7 +105,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 恢复任务
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -101,7 +115,7 @@ public class SysJobServiceImpl implements ISysJobService
         Long jobId = job.getJobId();
         String jobGroup = job.getJobGroup();
         job.setStatus(ScheduleConstants.Status.NORMAL.getValue());
-        int rows = jobMapper.updateJob(job);
+        int rows = jobRepository.updateJob(job);
         if (rows > 0)
         {
             scheduler.resumeJob(ScheduleUtils.getJobKey(jobId, jobGroup));
@@ -111,7 +125,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 删除任务后，所对应的trigger也将被删除
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -120,7 +134,7 @@ public class SysJobServiceImpl implements ISysJobService
     {
         Long jobId = job.getJobId();
         String jobGroup = job.getJobGroup();
-        int rows = jobMapper.deleteJobById(jobId);
+        int rows = jobRepository.deleteJobById(jobId);
         if (rows > 0)
         {
             scheduler.deleteJob(ScheduleUtils.getJobKey(jobId, jobGroup));
@@ -130,7 +144,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 批量删除调度信息
-     * 
+     *
      * @param jobIds 需要删除的任务ID
      * @return 结果
      */
@@ -140,14 +154,14 @@ public class SysJobServiceImpl implements ISysJobService
     {
         for (Long jobId : jobIds)
         {
-            SysJob job = jobMapper.selectJobById(jobId);
+            SysJob job = jobRepository.selectJobById(jobId);
             deleteJob(job);
         }
     }
 
     /**
      * 任务调度状态修改
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -169,7 +183,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 立即运行任务
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -194,7 +208,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 新增任务
-     * 
+     *
      * @param job 调度信息 调度信息
      */
     @Override
@@ -202,7 +216,7 @@ public class SysJobServiceImpl implements ISysJobService
     public int insertJob(SysJob job) throws SchedulerException, TaskException
     {
         job.setStatus(ScheduleConstants.Status.PAUSE.getValue());
-        int rows = jobMapper.insertJob(job);
+        int rows = jobRepository.insertJob(job);
         if (rows > 0)
         {
             ScheduleUtils.createScheduleJob(scheduler, job);
@@ -212,7 +226,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 更新任务的时间表达式
-     * 
+     *
      * @param job 调度信息
      */
     @Override
@@ -220,7 +234,7 @@ public class SysJobServiceImpl implements ISysJobService
     public int updateJob(SysJob job) throws SchedulerException, TaskException
     {
         SysJob properties = selectJobById(job.getJobId());
-        int rows = jobMapper.updateJob(job);
+        int rows = jobRepository.updateJob(job);
         if (rows > 0)
         {
             updateSchedulerJob(job, properties.getJobGroup());
@@ -230,7 +244,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 更新任务
-     * 
+     *
      * @param job 任务对象
      * @param jobGroup 任务组名
      */
@@ -249,7 +263,7 @@ public class SysJobServiceImpl implements ISysJobService
 
     /**
      * 校验cron表达式是否有效
-     * 
+     *
      * @param cronExpression 表达式
      * @return 结果
      */
