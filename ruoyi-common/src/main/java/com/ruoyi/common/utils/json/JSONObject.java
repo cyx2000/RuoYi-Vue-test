2751c78118c7b47de14c3328f93b3dd164ec1ee3
winter123 <2453101190@qq.com>
2026-03-27 13:03:12 +0800

    修复json错误，主类没有实例化，普通内部类无法实例化，将JSONObject转成普通类

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/json/JSONObject.java
@@ -0,0 +1,72 @@
+package com.ruoyi.common.utils.json;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 封装操作json对象的类
+ *
+ * @author winter123
+ */
+public class JSONObject extends LinkedHashMap<String, Object> {
+    public JSONObject() {
+        super();
+    }
+
+    public JSONObject(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public JSONObject(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    public JSONObject(int initialCapacity, float loadFactor, boolean accessOrder) {
+        super(initialCapacity, loadFactor, accessOrder);
+    }
+
+    public JSONObject(Map<String, Object> map) {
+        super(map);
+    }
+
+    /**
+     * 获取字符串结果
+     *
+     * @param target
+     * @param inKey
+     * @return null或者字符串结果
+     */
+    public String getString(String inKey) {
+        Object value = super.get(inKey);
+        if(value instanceof String strValue) {
+            return strValue;
+        }
+        return null;
+    }
+
+    /**
+     * 获取long整型结果
+     *
+     * @param target
+     * @param key
+     * @return 返回默认值或者long结果
+     */
+    public long getLongValue(String key) {
+        return getLongValue(key, 0L);
+    }
+
+    public long getLongValue(String key, long defaultValue) {
+        Object value = super.get(key);
+        if (value == null) {
+            return defaultValue;
+        } else if (value instanceof Number numValue) {
+            return numValue.longValue();
+        } else if (value instanceof String strValue) {
+            String str = strValue.trim();
+            if (!str.isEmpty() && !"null".equalsIgnoreCase(str)) {
+                return str.indexOf(46) != -1 ? (long)Double.parseDouble(str) : Long.parseLong(str);
+            }
+        }
+        return defaultValue;
+    }
+}
